f7dbfc4757c81c3081f0eba424f940b9bb6ff5ce
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-25 01:53:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
 # Add your code here
+require 'pry'
+
+class Dog
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+
+  def self.print_all
+    @@all.each {|dog| puts dog.name}
+  end
+
+  def save
+    #binding.pry
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+
+
+
+
+
+end

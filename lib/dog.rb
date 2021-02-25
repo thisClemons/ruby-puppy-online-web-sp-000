af15f2afa2992dbcfa7981355c6a1ff0ca9e0e59
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-25 01:43:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # Add your code here
+class Dog
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def clear_all
+    @@all = []
+  end
+
+  def print_all
+    @@all.each {|dog|, puts dog}
+
+  end
+
+
+end
